f2d8cdac39f23164b0d005e6ed57bf0f24f1f300
CDsmen <1159230460@qq.com>
2023-03-13 12:43:25 +0800

    定义各种结构体类型

---
+++ service/types.go
@@ -0,0 +1,58 @@
+package service
+
+type Response struct {
+	StatusCode int32  `json:"status_code"`
+	StatusMsg  string `json:"status_msg,omitempty"`
+}
+
+type Video struct {
+	Id            int64  `json:"id,omitempty"`
+	Author        User   `json:"author" gorm:"-"`
+	PlayUrl       string `json:"play_url"`
+	CoverUrl      string `json:"cover_url"`
+	FavoriteCount int64  `json:"favorite_count"`
+	CommentCount  int64  `json:"comment_count"`
+	IsFavorite    bool   `json:"is_favorite"`
+	Title 		  string `json:"title"`
+
+	Userid     int64  `json:"-"`
+	CreateTime int64 `json:"-"`
+}
+
+type Comment struct {
+	Id         int64  `json:"id,omitempty"`
+	User       User   `json:"user" gorm:"-"`
+	Content    string `json:"content,omitempty"`
+	CreateDate string `json:"create_date,omitempty"`
+
+	Userid int64 `json:"-"`
+}
+
+type User struct {
+	Id            int64  `json:"id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	FollowCount   int64  `json:"follow_count,omitempty"`
+	FollowerCount int64  `json:"follower_count,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty"`
+
+	TotalFavorited string `json:"total_favorited,omitempty"`
+	WorkCount      int64  `json:"work_count,omitempty"`
+	FavoriteCount  int64  `json:"favorite_count,omitempty"`
+}
+
+type Message struct {
+	Id         int64  `json:"id,omitempty"`
+	Content    string `json:"content,omitempty"`
+	CreateTime string `json:"create_time,omitempty"`
+}
+
+type MessageSendEvent struct {
+	UserId     int64  `json:"user_id,omitempty"`
+	ToUserId   int64  `json:"to_user_id,omitempty"`
+	MsgContent string `json:"msg_content,omitempty"`
+}
+
+type MessagePushEvent struct {
+	FromUserId int64  `json:"user_id,omitempty"`
+	MsgContent string `json:"msg_content,omitempty"`
+}
