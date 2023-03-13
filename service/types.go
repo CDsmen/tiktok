148da9627876de6cb8ba3a45f559527b1db89b7f
CDsmen <1159230460@qq.com>
2023-03-13 21:10:40 +0800

    定义各种结构体类型

--- service/types.go
+++ service/types.go
@@ -13,9 +13,9 @@ type Video struct {
 	FavoriteCount int64  `json:"favorite_count" gorm:"-"`
 	CommentCount  int64  `json:"comment_count" gorm:"-"`
 	IsFavorite    bool   `json:"is_favorite"`
-	Title 		  string `json:"title"`
+	Title         string `json:"title"`
 
-	Userid     int64  `json:"-"`
+	Userid     int64 `json:"-"`
 	CreateTime int64 `json:"-"`
 }
 
@@ -35,9 +35,9 @@ type User struct {
 	FollowerCount int64  `json:"follower_count,omitempty"`
 	IsFollow      bool   `json:"is_follow,omitempty"`
 
-	TotalFavorited string `json:"total_favorited,omitempty"`
-	WorkCount      int64  `json:"work_count,omitempty"`
-	FavoriteCount  int64  `json:"favorite_count,omitempty"`
+	TotalFavorited string `json:"total_favorited,omitempty" gorm:"-"`
+	WorkCount      int64  `json:"work_count,omitempty" gorm:"-"`
+	FavoriteCount  int64  `json:"favorite_count,omitempty" gorm:"-"`
 }
 
 type Message struct {
