732f86ff92517d372920dd557641342015aa3d82
CDsmen <1159230460@qq.com>
2023-03-14 10:45:10 +0800

    dao层favorite模块

---
+++ dao/favorite.go
@@ -0,0 +1,12 @@
+package dao
+
+func Favorite_add(user_id int64, video_id int64) error {
+	err := DB.Exec("CALL add_favorite(?, ?)", user_id, video_id).Error
+	return err
+}
+
+func Favorite_del(user_id int64, video_id int64) error {
+	err := DB.Exec("CALL del_favorite(?, ?)", user_id, video_id).Error
+	return err
+}
+
