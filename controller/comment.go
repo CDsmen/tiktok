d86edc818add32a12e1a00afdd185ef287a3d1f1
CDsmen <1159230460@qq.com>
2023-03-14 22:14:47 +0800

    CommentAction功能

--- controller/comment.go
+++ controller/comment.go
@@ -32,3 +32,59 @@ func CommentList(c *gin.Context) {
 		CommentList: commentList,
 	})
 }
+func CommentAction(c *gin.Context) {
+	token := c.Query("token")
+	videoId := c.Query("video_id")
+	actionType := c.Query("action_type")
+	text := c.Query("comment_text")
+	commentId := c.Query("comment_id")
+
+	if actionType == "1" {
+		videoid_int64, err := strconv.ParseInt(videoId, 10, 64)
+		if err != nil {
+			c.JSON(http.StatusOK, UserResponse{
+				Response: Response{StatusCode: 1, StatusMsg: "videoId err"},
+			})
+			return
+		}
+		var comment service.Comment
+		err = service.AddComment(videoid_int64, text, token, &comment)
+		if err != nil {
+			c.JSON(http.StatusOK, UserResponse{
+				Response: Response{StatusCode: 1, StatusMsg: "AddComment err"},
+			})
+			return
+		} else {
+			c.JSON(http.StatusOK, CommentListResponse{
+				Response:    Response{StatusCode: 0},
+				CommentList: []service.Comment{comment},
+			})
+			return
+		}
+	} else if actionType == "2" {
+		commentid_int64, err := strconv.ParseInt(commentId, 10, 64)
+		if err != nil {
+			c.JSON(http.StatusOK, UserResponse{
+				Response: Response{StatusCode: 1, StatusMsg: "commentId err"},
+			})
+			return
+		}
+		err = service.DelComment(commentid_int64, token)
+		if err != nil {
+			c.JSON(http.StatusOK, UserResponse{
+				Response: Response{StatusCode: 1, StatusMsg: "DelComment err"},
+			})
+			return
+		} else {
+			c.JSON(http.StatusOK, CommentActionResponse{
+				Response: Response{StatusCode: 0, StatusMsg: "Delete succeeded"},
+			})
+			return
+		}
+	} else {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "actionType err"},
+		})
+		return
+	}
+}
--- service/comment.go
+++ service/comment.go
@@ -26,12 +26,48 @@ func CommentList(video_id int64, token string, commentsList *[]Comment) error {
 
 	// 补充user
 	for id := range *commentsList {
-		var user User
 		err = UserInfo(strconv.FormatInt((*commentsList)[id].Userid, 10), token, &(*commentsList)[id].User)
 		if err != nil {
 			return err
 		}
-		(*commentsList)[id].User = user
+	}
+	return nil
+}
+
+func AddComment(video_id int64, text string, token string, comment *Comment) error {
+	// token不存在
+	err := myjwt.FindToken(token)
+	if err != nil {
+		return err
+	}
+
+	// 解析token
+	claim, err := myjwt.VerifyAction(token)
+	if err != nil {
+		return err
+	}
+	err = dao.Comment_add(video_id, claim.UserID, text, comment)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DelComment(comment_id int64, token string) error {
+	// token不存在
+	err := myjwt.FindToken(token)
+	if err != nil {
+		return err
+	}
+
+	// 解析token
+	_, err = myjwt.VerifyAction(token)
+	if err != nil {
+		return err
+	}
+	err = dao.Comment_del(comment_id)
+	if err != nil {
+		return err
 	}
 	return nil
 }
